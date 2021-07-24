09575e4fe1c8fbb94e442a08c18ea4337f1e6264
SirEliot <52104612+SirEliot@users.noreply.github.com>
2021-07-24 23:20:17 +0300

    Final Commit

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,36 @@
 #!/bin/bash
+
 #add fix to exercise6-fix here
+
+if [ "$#" -lt 2 ]; then	
+	exit 1
+fi
+
+
+host=$(hostname | cut -c 7)
+output="${@: -1}"
+arg_num=1
+bytes=0
+
+if [ "$host" -eq 1 ]; then
+		remote_host="server2"
+else
+		remote_host="server1"
+fi
+
+
+for item in "$@"
+do
+	if [ "$arg_num" -lt "$#" ]; then
+		# echo $arg_num - $item
+		sudo scp -qi /home/vagrant/.ssh/id_rsa $item vagrant@$remote_host:$output
+		let bytes+=$(stat --format=%s $item)
+		
+	fi
+	let arg_num++
+done
+
+echo $bytes
+ 
+
+
